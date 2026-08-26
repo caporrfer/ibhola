5470c570d3fe6273516497dc176fb6cb070b7631
Carlos <caporrfer@gmail.com>
2026-08-26 12:29:17 +0000

    Restore site to c394af0

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  themeColor: "#77A737",
+  themeColor: "#25352f",
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
